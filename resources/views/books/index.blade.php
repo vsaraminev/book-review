fb4e94da944c32601a7527507dc1d9e8f6d5e11c
Ventsislav Saraminev <v.saraminev@abv.bg>
2025-01-21 21:09:50 +0200

    Filter books by period

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,10 +3,25 @@
 @section('content')
     <h1 class="mb-10 text-2xl">Books</h1>
     <form class="flex gap-2 mb-4" method="GET" action="{{route('books.index')}}">
-        <input type="text" class="input" name="title" placeholder="Search by title" value="{{ request('title') }}">
+        <input type="text" class="input" name="title" placeholder="Search by title" value="{{ request('title') }}"/>
+        <input type="hidden" name="filter" value="{{ request('filter') }}"/>
         <button class="btn">Search</button>
         <a class="btn" href="{{ route('books.index') }}">Clear</a>
     </form>
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+            '' => 'Latest',
+            'popular_last_month' => 'Popular Last Month',
+            'popular_last_6months' => 'Popular Last 6 Months',
+            'highest_rated_last_month' => 'Highest Rated Last Month',
+            'highest_rated_last_6months' => 'Highest Rated Last 6 Months'
+            ];
+        @endphp
+        @foreach($filters as $key => $label)
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}" class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item'}}">{{ $label }}</a>
+        @endforeach
+    </div>
     <ul>
         @forelse($books as $book)
         <li class="mb-4">
@@ -19,7 +34,7 @@
                 </div>
                 <div>
                   <div class="book-rating">
-                    {{number_format($book->reviews_avg_raiting, 1)}}
+                    {{number_format($book->reviews_avg_rating, 1)}}
                   </div>
                   <div class="book-review-count">
                     out of {{$book->reviews_count}} {{ \Illuminate\Support\Str::plural('review', $book->reviews_count)}}
