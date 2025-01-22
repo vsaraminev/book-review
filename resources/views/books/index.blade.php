907bf75ec5adc72110c47fc56333c40c6ca0f213
Ventsislav Saraminev <v.saraminev@abv.bg>
2025-01-22 13:49:19 +0200

    Added Reviews functionality

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,52 +2,53 @@
 
 @section('content')
     <h1 class="mb-10 text-2xl">Books</h1>
-    <form class="flex gap-2 mb-4" method="GET" action="{{route('books.index')}}">
-        <input type="text" class="input" name="title" placeholder="Search by title" value="{{ request('title') }}"/>
-        <input type="hidden" name="filter" value="{{ request('filter') }}"/>
+    <form class="flex gap-2 mb-4" method="GET" action="{{ route('books.index') }}">
+        <input type="text" class="input" name="title" placeholder="Search by title" value="{{ request('title') }}" />
+        <input type="hidden" name="filter" value="{{ request('filter') }}" />
         <button class="btn">Search</button>
         <a class="btn" href="{{ route('books.index') }}">Clear</a>
     </form>
     <div class="filter-container mb-4 flex">
         @php
             $filters = [
-            '' => 'Latest',
-            'popular_last_month' => 'Popular Last Month',
-            'popular_last_6months' => 'Popular Last 6 Months',
-            'highest_rated_last_month' => 'Highest Rated Last Month',
-            'highest_rated_last_6months' => 'Highest Rated Last 6 Months'
+                '' => 'Latest',
+                'popular_last_month' => 'Popular Last Month',
+                'popular_last_6months' => 'Popular Last 6 Months',
+                'highest_rated_last_month' => 'Highest Rated Last Month',
+                'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
             ];
         @endphp
-        @foreach($filters as $key => $label)
-            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}" class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item'}}">{{ $label }}</a>
+        @foreach ($filters as $key => $label)
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+                class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">{{ $label }}</a>
         @endforeach
     </div>
     <ul>
         @forelse($books as $book)
-        <li class="mb-4">
-            <div class="book-item">
-              <div
-                class="flex flex-wrap items-center justify-between">
-                <div class="w-full flex-grow sm:w-auto">
-                  <a href="{{ route('books.show', $book)}}" class="book-title">{{ $book->title}}</a>
-                  <span class="book-author">by {{$book->author}}</span>
+            <li class="mb-4">
+                <div class="book-item">
+                    <div class="flex flex-wrap items-center justify-between">
+                        <div class="w-full flex-grow sm:w-auto">
+                            <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+                            <span class="book-author">by {{ $book->author }}</span>
+                        </div>
+                        <div>
+                            <div class="book-rating">
+                                <x-star-rating :rating="$book->reviews_avg_rating" />
+                            </div>
+                            <div class="book-review-count">
+                                out of {{ $book->reviews_count }}
+                                {{ \Illuminate\Support\Str::plural('review', $book->reviews_count) }}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                  <div class="book-rating">
-                    {{number_format($book->reviews_avg_rating, 1)}}
-                  </div>
-                  <div class="book-review-count">
-                    out of {{$book->reviews_count}} {{ \Illuminate\Support\Str::plural('review', $book->reviews_count)}}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
+            </li>
         @empty
             <li class="mb-4">
                 <div class="empty-book-item">
-                  <p class="empty-text">No books found</p>
-                  <a href="{{ route('books.index')}}" class="reset-link">Reset criteria</a>
+                    <p class="empty-text">No books found</p>
+                    <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
                 </div>
             </li>
         @endforelse
